a1048dadd52bcc1001b455fe8999ccc086fc96a5
gergopistai <gergopistai@gmail.com>
2021-04-13 14:48:02 +0200

    Moved drill buttons to constructor
    Changed them from pointers to objects.
    Moved constants from constructor into the class itself.

--- drills/Main_window.cpp
+++ drills/Main_window.cpp
@@ -9,62 +9,62 @@ class Main_window : Window
 {
 public:
 	Main_window(Point xy, int w, int h, const string& title) :
-		Window(xy, w, h, title)
+		Window(xy, w, h, title),
+		d12{ Point{ drill_pos_x, 5 + d_separ++ }, drill_x, drill_y, "Drill 12",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(12); } },
+		d13{ Point{ drill_pos_x, 25 + d_separ++ }, drill_x, drill_y, "Drill 13",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(13); } },
+		d14{ Point{ drill_pos_x, 45 + d_separ++ }, drill_x, drill_y, "Drill 14",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(14); } },
+		d15{ Point{ drill_pos_x, 65 + d_separ++ }, drill_x, drill_y, "Drill 15",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(15); } },
+		d16{ Point{ drill_pos_x, 85 + d_separ++ }, drill_x, drill_y, "Drill 16",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(16); } },
+		d19{ Point{ drill_pos_x, 105 + d_separ++ }, drill_x, drill_y, "Drill 19",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(19); } },
+		d20{ Point{ drill_pos_x, 125 + d_separ++ }, drill_x, drill_y, "Drill 20",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(20); } },
+		d21{ Point{ drill_pos_x, 145 + d_separ++ }, drill_x, drill_y, "Drill 21",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(21); } },
+		d27{ Point{ drill_pos_x, 165 + d_separ++ }, drill_x, drill_y, "Drill 27",
+		[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(27); } },
+		ex12{ Point{80, 5}, 80, 20, "Exercise 12",
+			[](Address, Address pw) {reference_to<Main_window>(pw).start_exercise(12); } },
+		exit_btn{ Point{ x_max() - 75, 5}, 70, 20, "Exit",
+			[](Address, Address pw) {reference_to<Main_window>(pw).exit(); } }
 	{
-		// Drills
-		constexpr int drill_x = 70;
-		constexpr int drill_y = 20;
-		constexpr int drill_pos_x = 5;
-		int d_separ = 0;
-
-		Button* d12 = new Button{ Point{ drill_pos_x, 5 + d_separ++ }, drill_x, drill_y, "Drill 12",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(12); } };
-		attach(*d12);
-
-		Button* d13 = new Button{ Point{ drill_pos_x, 25 + d_separ++ }, drill_x, drill_y, "Drill 13",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(13); } };
-		attach(*d13);
-
-		Button* d14 = new Button{ Point{ drill_pos_x, 45 + d_separ++ }, drill_x, drill_y, "Drill 14",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(14); } };
-		attach(*d14);
-
-		Button* d15 = new Button{ Point{ drill_pos_x, 65 + d_separ++ }, drill_x, drill_y, "Drill 15",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(15); } };
-		attach(*d15);
-
-		Button* d16 = new Button{ Point{ drill_pos_x, 85 + d_separ++ }, drill_x, drill_y, "Drill 16",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(16); } };
-		attach(*d16);
-
-		Button* d19 = new Button{ Point{ drill_pos_x, 105 + d_separ++ }, drill_x, drill_y, "Drill 19",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(19); } };
-		attach(*d19);
-
-		Button* d20 = new Button{ Point{ drill_pos_x, 125 + d_separ++ }, drill_x, drill_y, "Drill 20",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(20); } };
-		attach(*d20);
-
-		Button* d21 = new Button{ Point{ drill_pos_x, 145 + d_separ++ }, drill_x, drill_y, "Drill 21",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(21); } };
-		attach(*d21);
-
-		Button* d27 = new Button{ Point{ drill_pos_x, 165 + d_separ++ }, drill_x, drill_y, "Drill 27",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_drill(27); } };
-		attach(*d27);
-
-		// Exercises
-		Button* ex12 = new Button{ Point{80, 5}, 80, 20, "Exercise 12",
-			[](Address, Address pw) {reference_to<Main_window>(pw).start_exercise(12); } };
-		attach(*ex12);
-
-		// Other buttons
-		Button* exit = new Button{ Point{ x_max() - 75, 5}, 70, 20, "Exit",
-			[](Address, Address pw) {reference_to<Main_window>(pw).exit(); } };
-		attach(*exit);
+		// Attaching buttons
+		attach(d12);
+		attach(d13);
+		attach(d14);
+		attach(d15);
+		attach(d16);
+		attach(d19);
+		attach(d20);
+		attach(d21);
+		attach(d27);
+		attach(ex12);
+		attach(exit_btn);
 	}
 
 private:
+	const int drill_x = 70;
+	const int drill_y = 20;
+	const int drill_pos_x = 5;
+	int d_separ = 0;
+
+	Button d12;
+	Button d13;
+	Button d14;
+	Button d15;
+	Button d16;
+	Button d19;
+	Button d20;
+	Button d21;
+	Button d27;
+	Button ex12;
+	Button exit_btn;
+
 	void exit()
 	{
 		hide();
@@ -78,10 +78,12 @@ private:
 			{
 				Exercise12 e12;
 				e12.start();
+				break;
 			}
 			default:
 			{
 				cout << "Not implemented yet." << endl;
+				break;
 			}
 		}
 	}
